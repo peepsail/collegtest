729f359203e45ea617e14cea82634cd8c310ae32
peepsail <46482345+peepsail@users.noreply.github.com>
2019-08-18 19:15:06 +1200

    Update small_individual.js

--- small_individual.js
+++ small_individual.js
@@ -1,1 +1,11 @@
-test
+(function(H) {
+
+    Highcharts.SVGRenderer.prototype.symbols.individual = function(x, y, w, h, r) {
+        return [
+            'M', x+w*(2/5), y+h/2, 
+            'A', w/3, w/3, 0,1,1, x+(w*3/5),y+h/2 ,
+            'L', x+(w*4/5), y+h,
+            x+(w*1/5),y+h
+        ];
+    };
+}(Highcharts));
